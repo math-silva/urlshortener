38148d653ffd3fdf63f79b0845544ffbd0fd5b0f
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-05 19:50:06 -0300

    refactor: update links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,31 @@
 class LinksController < ApplicationController
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      flash[:shortened_url] = @link.short
+    
+    redirect_to :new
+    end
+  end  
+  
   def show
-    @link = Link.find_by_slug(params[:slug]) 
-    render 'errors/404', status: 404 if @link.nil?
-    @link.update_attribute(:clicked, @link.clicked + 1)
-    redirect_to @link.url, allow_other_host: true
+    @link = Link.find_by(slug: params[:slug])
+    if @link.nil?
+      flash.now[:alert] = "The requested shortened link '#{params[:slug]}' could not be found."
+      render 'errors/404', status: 404
+    else
+      @link.increment!(:clicked)
+      redirect_to @link.url, allow_other_host: true
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :slug)
   end
 end
